9f421d226ca936f04503909453b6c7c7ea30636e
gaughan <ggaughan@gmail.com>
2015-03-10 22:26:23 +0000

    line test

--- app/src/androidTest/java/com/example/android/sunshine/app/FullTestSuite.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/FullTestSuite.java
@@ -24,6 +24,7 @@ public class FullTestSuite extends TestSuite {
     public static Test suite() {
         return new TestSuiteBuilder(FullTestSuite.class)
                 .includeAllPackagesUnderHere().build();
+        
     }
 
     public FullTestSuite() {
